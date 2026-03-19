42c023d75d2cd013aff706c81c6ea0f7be5cab72
tulinhokkj <anderson.macedo@nave.org.br>
2026-03-19 10:38:10 -0300

    ta feito

--- ex-02-listas-metodos.js
+++ ex-02-listas-metodos.js
@@ -221,7 +221,7 @@ Envie a lista animais modificada na resposta10.
 // Operações necessárias e resposta a partir da linha abaixo
 let s1 = ["carcará", "morcego", "salmão"]
 
-export const resposta10 = false
+export const resposta10 = animais.concat(s1)
 
 /* Questão 11
 Crie (usando const) uma lista vazia chamada "pedras", e use nela o método concat
@@ -231,7 +231,12 @@ reverse nessa lista, e após isso use o método push para adicionar os itens
 final na resposta11.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta11 = false
+let pedras = []
+pedras.concat("quartzo", "basalto", "granito")
+pedras.reverse()
+pedras.concat("calcário", "mármore", "mica")
+pedras.reverse()
+export const resposta11 = pedras
 
 /* Questão 12
 Continuando com a lista criada na questão 11, use o método unshift para adicionar
@@ -240,7 +245,11 @@ questão. Use o método "concat" para adicionar os itens "topázio", "lazurita" 
 "ônix" ao final desta lista, e retorne-a na resposta12.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta12 = false
+pedras.unshift("jade")
+pedras.unshift("obsidiana")
+pedras.unshift("zircônio")
+pedras = pedras.concat("topázio", "lazurita", "ônix")
+export const resposta12 = pedras
 
 /* Questão 13
 Seguindo com a lista de pedras, use os métodos shift e pop para mover os três
@@ -248,12 +257,17 @@ primeiros itens para o final da lista. Após isso, aplique o método reverse, e
 envie a lista na resposta13.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta13 = false
+pedras.push(pedras.shift)
+pedras.push(pedras.shift)
+pedras.push(pedras.shift)
+pedras.reverse()
+export const resposta13 = pedras
 
 /* Questão 14
 Finalizando com a lista de pedras, use o método pop para remover os 6 itens finais
 da lista e armazene-os em uma lista nova. Envie esta lista nova na resposta 14.
 */
 // Operações necessárias e resposta a partir da linha abaixo
-export const resposta14 = false
+let g = [pedras.pop(), pedras.pop(), pedras.pop(), pedras.pop(), pedras.pop(), pedras.pop()]
+export const resposta14 = g
 
