099b252bc58dd3bd56e1694a57fe003f22936746
Shini <carlos.benjamin@nave.org.br>
2026-03-19 09:44:57 -0300

    na

--- ex-01-listas-indices.js
+++ ex-01-listas-indices.js
@@ -234,3 +234,5 @@ Finalizando com a lista da questão 7, crie uma nova lista com três itens onde:
 // Operações necessárias e resposta a partir da linha abaixo
 export const resposta10 = false
 
+
+//NaNa
